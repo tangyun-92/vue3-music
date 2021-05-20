5ceb382925dfb1709b226ef5c83c723763858f2f
唐云 <tangyun1992@qq.com>
2021-05-20 10:47:13 +0800

    引入antd-vue有normalize

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
+import 'normalize.css/normalize.css'
 
-createApp(App).use(store).use(router).mount("#app");
+createApp(App).use(store).use(router).use(Antd).mount('#app')
